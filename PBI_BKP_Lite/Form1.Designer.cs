34392a8b5adf70ea16d7b88858ffda5ea62b5894
inegocios <git@inegocios.cl>
2021-04-28 12:03:00 -0400

    Agregar archivos de proyecto.

---
+++ PBI_BKP_Lite/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace PBI_BKP_Lite
+{
+    partial class frmPBIBKP
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.lblPathDestino = new System.Windows.Forms.Label();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.btnSalir = new FontAwesome.Sharp.IconButton();
+            this.SuspendLayout();
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // lblPathDestino
+            // 
+            this.lblPathDestino.AutoSize = true;
+            this.lblPathDestino.Location = new System.Drawing.Point(33, 43);
+            this.lblPathDestino.Name = "lblPathDestino";
+            this.lblPathDestino.Size = new System.Drawing.Size(0, 17);
+            this.lblPathDestino.TabIndex = 0;
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.FileExport;
+            this.iconButton1.IconColor = System.Drawing.Color.Black;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.Location = new System.Drawing.Point(163, 146);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(189, 72);
+            this.iconButton1.TabIndex = 1;
+            this.iconButton1.Text = "Iniciar";
+            this.iconButton1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.iconButton1.UseVisualStyleBackColor = true;
+            this.iconButton1.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.IconChar = FontAwesome.Sharp.IconChar.SignOutAlt;
+            this.btnSalir.IconColor = System.Drawing.Color.Black;
+            this.btnSalir.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnSalir.Location = new System.Drawing.Point(491, 146);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(189, 72);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmPBIBKP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 248);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.iconButton1);
+            this.Controls.Add(this.lblPathDestino);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmPBIBKP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Power BI Backup - Lite";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label lblPathDestino;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton btnSalir;
+    }
+}
+
---
+++ PBI_BKP_Lite/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PBI_BKP_Lite
+{
+    public partial class frmPBIBKP : Form
+    {
+        string PathDestino;
+        string usernameProceso;
+        string TenantId;
+        string IdReporte = "";
+        string NombreReporte = "";
+        string ArchivoRepo = "";
+        int numero = 1;
+        string IdDataFlow = "";
+        string NombreDataFlow = "";
+
+        public frmPBIBKP()
+        {
+            InitializeComponent();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var fldrDlg = new FolderBrowserDialog())
+            {
+                if (fldrDlg.ShowDialog() == DialogResult.OK)
+                {
+                    PathDestino = fldrDlg.SelectedPath;
+                    lblPathDestino.Text = "Destino de Respaldos:   " + PathDestino;
+                }
+                else
+                {
+                    MessageBox.Show("No se seleccionó Destino de Respaldos", "PBI Backup - Lite", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    System.Windows.Forms.Application.Exit();
+                }
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult boton = MessageBox.Show("¿Está seguro que desea salir?", "PBI Backup - Lite", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            if (boton == DialogResult.OK)
+            {
+                System.Windows.Forms.Application.Exit();
+            }
+        }
+
+        #region Revisa si archivo esta bloqueado
+
+        protected virtual bool IsFileLocked(FileInfo file)
+        {
+            try
+            {
+                using (FileStream stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    stream.Close();
+                }
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+
+            //file is not locked
+            return false;
+        }
+        #endregion Revisa si archivo esta bloqueado
+
+        #region saca valor de la linea
+
+        protected virtual string Contenido(string Evaluar, int desde)
+        {
+            return Evaluar.Substring(desde,Evaluar.Length - desde); //Despeja el contenido de la linea, solo lo que es realmente utilizable
+        }
+
+        #endregion saca valor de la linea
+
+        protected virtual void EsperaCreacionArchivo(string Archivo)
+        {
+            while (!File.Exists(Archivo))
+            {
+                //espera mintras se crea el archivo 
+            }
+        }
+
+        protected void EsperaDesbloqueoArchivo(string Archivo)
+        {
+            while (IsFileLocked(new FileInfo(Archivo)))
+            {
+                //espera mientras se desocupa el archivo 
+            }
+        }
+
+        protected void BorraArchivo(string Archivo)
+        {
+            #region Elimina Archivos  
+
+            Process cmdRM = new Process();
+            cmdRM.StartInfo.FileName = "powershell.exe";
+            cmdRM.StartInfo.RedirectStandardInput = true;
+            cmdRM.StartInfo.RedirectStandardOutput = true;
+            cmdRM.StartInfo.CreateNoWindow = true;
+            cmdRM.StartInfo.UseShellExecute = false;
+            cmdRM.Start();
+
+            cmdRM.StandardInput.WriteLine(@"rm " + Archivo);
+
+            cmdRM.StandardInput.Flush();
+            cmdRM.StandardInput.Close();
+
+            cmdRM.WaitForExit();
+
+            #endregion  Elimina Archivos  
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+            string ArchivoLogin = PathDestino + "\\login.txt";
+            string ArchivoWorkspace = PathDestino + "\\Workspaces.txt";
+            string ArchivoReportes = PathDestino + "\\Reportes_";
+            string ArchivoDF = PathDestino + "\\DF_";
+
+            string[] textLogin;
+
+            #region Limpia el directorio de salida
+
+            BorraArchivo(PathDestino + "\\*.txt");
+            BorraArchivo(PathDestino + "\\*.pbix");
+            BorraArchivo(PathDestino + "\\*.json");
+
+            #endregion Limpia el directorio de salida
+
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "powershell.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.EnableRaisingEvents = true;
+            cmd.Start();
+            cmd.BeginOutputReadLine();
+
+
+            #region Login a Power BI
+
+            cmd.StandardInput.WriteLine(@"Login-PowerBI > " + ArchivoLogin);
+
+            EsperaCreacionArchivo(ArchivoLogin);
+            EsperaDesbloqueoArchivo(ArchivoLogin);
+
+            if (new FileInfo(ArchivoLogin).Length == 0)  //Se crea correctamente la sesion?
+            {
+                MessageBox.Show("Error en Login a Power BI, por favor intente más tarde", "PBI Backup - Lite", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                System.Windows.Forms.Application.Exit();
+            }
+
+            textLogin = File.ReadAllLines(ArchivoLogin);
+
+            foreach (string line in textLogin)
+            {
+                string usernameLogin = "UserName";
+                string IdLogin = "Id";
+                string separador = ": ";
+
+                if (line.Contains(IdLogin))
+                {
+                    if (line.Contains(separador))
+                    {
+                        int index = line.IndexOf(separador);
+                        if (index >= 0)
+                        {
+                            TenantId = Contenido(line, index + separador.Length); // Saca Id Usuario
+                        }
+                    }
+                }
+
+                if (line.Contains(usernameLogin))
+                {
+                    if (line.Contains(separador))
+                    {
+                        int index = line.IndexOf(separador);
+                        if (index >= 0)
+                        {
+                            usernameProceso = Contenido(line, index + separador.Length);  // Saca username Usuario
+                            MessageBox.Show("Bienvenido: " + usernameProceso);
+                        }
+                    }
+                }
+            }
+
+            #endregion  Login a Power BI
+
+            #region Obtiene los Workspaces del usuario
+
+            cmd.StandardInput.WriteLine(@"Get-PowerBIWorkspace -All > " + ArchivoWorkspace);
+
+            EsperaCreacionArchivo(ArchivoWorkspace);
+            EsperaDesbloqueoArchivo(ArchivoWorkspace);
+
+            string [] textWorkspace = File.ReadAllLines(ArchivoWorkspace);
+
+            ArrayList WorkspaceId = new ArrayList();
+
+            foreach (string line in textWorkspace)
+            {
+                string IdWorkspace = "Id                    :";
+                string separador = ": ";
+
+                if (line.Contains(IdWorkspace))
+                {
+                    if (line.Contains(separador))
+                    {
+                        int index = line.IndexOf(separador);
+                        if (index >= 0)
+                        {
+                            WorkspaceId.Add(Contenido(line, index + separador.Length));
+                        }
+                    }
+                }
+            }
+
+            #endregion Obtiene Listado de reportes de los Workspaces del usuario
+
+            #region Obtiene Reportes para backup
+
+            foreach(string IdWorkspace in WorkspaceId)
+            {
+                string ArchivoReportesWorkspace = ArchivoReportes + IdWorkspace +".txt";
+                cmd.StandardInput.WriteLine(@"Get-PowerBIReport -WorkspaceId " + IdWorkspace + " > " + ArchivoReportesWorkspace);
+                EsperaCreacionArchivo(ArchivoReportesWorkspace);
+                EsperaDesbloqueoArchivo(ArchivoReportesWorkspace);
+
+                if (new FileInfo(ArchivoReportesWorkspace).Length > 0)  //Existen Reportes en el Workspace?
+                {
+                    string[] textReporte = File.ReadAllLines(ArchivoReportesWorkspace);
+
+                    foreach (string line in textReporte)
+                    {
+                        string IdRep = "Id        :";
+                        string Nombre = "Name      :";
+                        string separador = ": ";
+                        
+
+                        if (line.Contains(IdRep))
+                        {
+                            int index = line.IndexOf(separador);
+                            if (index >= 0)
+                            {
+                                IdReporte = Contenido(line, index + separador.Length);
+                            }
+                        }
+
+                        if (line.Contains(Nombre))
+                        {
+                            int index = line.IndexOf(separador);
+                            if (index >= 0)
+                            {
+                                NombreReporte = Contenido(line, index + separador.Length);
+                                NombreReporte = Regex.Replace(NombreReporte.Normalize(NormalizationForm.FormD), @"[^a-zA-Z0-9 ]+", "");
+                            }
+                        }
+
+                        if ((NombreReporte.Length > 0) && (IdReporte.Length > 0))
+                        {
+                            string Timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds().ToString();
+                            ArchivoRepo = PathDestino + "\\" + NombreReporte + "_" + Timestamp + numero.ToString() + ".pbix";
+                            numero++;
+                            cmd.StandardInput.WriteLine(@"Export-PowerBIReport -Id " + IdReporte + " -OutFile \"" + ArchivoRepo + "\" -WorkspaceId " + IdWorkspace);
+                            Console.WriteLine("Creando archivo: " + ArchivoRepo);
+                           // EsperaCreacionArchivo(ArchivoRepo);
+                           // EsperaDesbloqueoArchivo(ArchivoRepo);
+                           
+                            IdReporte = NombreReporte = "";
+                        }
+                        
+                    }
+                }
+
+                string ArchivoDataFlowWorkspace = ArchivoDF + IdWorkspace + ".txt";
+                cmd.StandardInput.WriteLine(@"Get-PowerBIDataflow -WorkspaceId " + IdWorkspace + " > " + ArchivoDataFlowWorkspace);
+                EsperaCreacionArchivo(ArchivoDataFlowWorkspace);
+                EsperaDesbloqueoArchivo(ArchivoDataFlowWorkspace);
+
+                if (new FileInfo(ArchivoDataFlowWorkspace).Length > 0)  //Existen Reportes en el Workspace?
+                {
+                    string[] textDF = File.ReadAllLines(ArchivoDataFlowWorkspace);
+
+                    foreach (string line in textDF)
+                    {
+                        string IdDF = "Id           :";
+                        string NombreDF = "Name         :";
+                        string separador = ": ";
+
+                        if (line.Contains(IdDF))
+                        {
+                            int index = line.IndexOf(separador);
+                            if (index >= 0)
+                            {
+                                IdDataFlow = Contenido(line, index + separador.Length);
+                            }
+                        }
+
+                        if (line.Contains(NombreDF))
+                        {
+                            int index = line.IndexOf(separador);
+                            if (index >= 0)
+                            {
+                                NombreDataFlow = Contenido(line, index + separador.Length);
+                                //NombreDataFlow = Regex.Replace(NombreDataFlow.Normalize(NormalizationForm.FormD), @"[^a-zA-Z0-9 ] + ", "");
+                                NombreDataFlow = Regex.Replace(NombreDataFlow.Normalize(NormalizationForm.FormD), @"[^\w]", "");
+                            }
+                        }
+
+                        if ((NombreDataFlow.Length > 0) && (IdDataFlow.Length > 0))
+                        {
+                            string Timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds().ToString();
+                            ArchivoDF = PathDestino + "\\" + NombreDataFlow + "_" + Timestamp + numero.ToString() + ".json";
+                            numero++;
+                            cmd.StandardInput.WriteLine(@"Export-PowerBIDataflow -Id " + IdDataFlow + " -OutFile \"" + ArchivoDF + "\" -WorkspaceId " + IdWorkspace);
+                            Console.WriteLine("Creando archivo: " + ArchivoDF);
+                            // EsperaCreacionArchivo(ArchivoRepo);
+                            // EsperaDesbloqueoArchivo(ArchivoRepo);
+
+                            IdDataFlow = NombreDataFlow = "";
+                        }
+                    }
+
+                }
+            }
+
+            #endregion Obtiene Reportes para backup
+
+
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+
+            cmd.WaitForExit();
+
+            #region Limpia archivos Temporales
+
+            BorraArchivo(PathDestino + "\\*.txt");
+
+            #endregion Limpia archivos Temporales
+
+            MessageBox.Show("Proceso ha terminado correctamente.");
+        }
+    }
+}
